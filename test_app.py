0d410275ca86443cf6b1006929b3ea68e343784a
Harsh sareen <harshsareen@harshs-MacBook-Pro.local>
2026-02-12 11:21:07 +0530

    pytest_decorators

--- test_app.py
+++ test_app.py
@@ -1,20 +1,40 @@
-import unittest
+import pytest
 from app import app
 
-class FlaskAppTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
 
-    def test_hello(self):
-        response = self.app.get('/hello')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.get_json(), {"message": "Hello, World!"})
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
 
-    def test_add(self):
-        response = self.app.post('/add', json={'a': 5, 'b': 3})
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.get_json(), {"result": 8})
+def test_home(client):
+    response = client.get('/hello')
+    assert response.status_code == 200
+    assert response.data == b"api is working "
 
-if __name__ == '__main__':
-    unittest.main()
+def test_add(client):
+    response = client.post('/add', json={'a': 5, 'b': 3})
+    assert response.status_code == 200
+    assert response.get_json() == {"result": 8}
+
+def test_add_invalid(client):
+    response = client.post('/add', json={'a': '5', 'b': 3})
+    assert response.status_code == 400
+# class FlaskAppTestCase(unittest.TestCase):
+#     def setUp(self):
+#         self.app = app.test_client()
+#         self.app.testing = True
+
+#     def test_hello(self):
+#         response = self.app.get('/hello')
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(response.get_json(), {"message": "Hello, World!"})
+
+#     def test_add(self):
+#         response = self.app.post('/add', json={'a': 5, 'b': 3})
+#         self.assertEqual(response.status_code, 200)
+#         self.assertEqual(response.get_json(), {"result": 8})
+
+# if __name__ == '__main__':
+#     unittest.main()
